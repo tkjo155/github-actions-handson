000c7bd600b3a314575d4b03476c664cf8588cf3
tkjo155 <takajo.sum15@gmail.com>
2025-10-27 15:51:27 +0900

    Add divide function

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,4 +1,4 @@
-const { add, subtract, multiply } = require('./calculator');
+const { add, subtract, multiply,divide } = require('./calculator');
 
 test('足し算が正しく動く', () => {
   expect(add(2, 3)).toBe(5);
@@ -10,4 +10,12 @@ test('引き算が正しく動く', () => {
 
 test('掛け算が正しく動く', () => {
   expect(multiply(3, 4)).toBe(12);
+});
+
+test(`割り算が正しく動く`,()=> {
+  expect(divide(10,2)).toBe(5);
+});
+
+test(`0で割るとエラーになる`,() => {
+  expect(divide(10,0)).toTrow(`0で割ることはできません`)
 });
